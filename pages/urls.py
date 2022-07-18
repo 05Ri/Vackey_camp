e83da2d4fc95a4ba1f0796cbfb84905ba86b7654
05Ri <yh980827@naver.com>
2022-07-18 13:09:13 +0900

    3주차 v0.3 완료

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.mainpage),
-    path('company/', views.company),
+    path('main/', views.mainpage),
+
+    path('company/', views.company_intro),
+    path('company/greeting/', views.company_greeting),
+    path('company/members/', views.company_members),
+    path('company/visit/', views.company_visit),
+
+    path('product/', views.product_intro),
+    path('mysite/', views.product_all),
+    path('product/popular/', views.product_popular),
+    path('product/sale/', views.product_sale),
+
+    path('contact/', views.contact_intro),
+    path('contact/FAQ/', views.contact_faq),
+    path('contact/QnA/', views.contact_qna),
+    path('contact/us/', views.contact_us),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,58 @@
 from django.shortcuts import render
 
 
+# 메인 페이지
 def mainpage(request):
     return render(request, 'pages/mainpage.html')
 
 
-def company(request):
-    return render(request, 'pages/company_info.html')
+# 회사 소개 관련
+def company_intro(request):
+    return render(request, 'pages/company/company_intro.html')
+
+
+def company_greeting(request):
+    return render(request, 'pages/company/company_greeting.html')
+
+
+def company_members(request):
+    return render(request, 'pages/company/company_members.html')
+
+
+def company_visit(request):
+    return render(request, 'pages/company/company_visit.html')
+
+
+# 상품 소개 관련
+def product_intro(request):
+    return render(request, 'pages/product/product_intro.html')
+
+
+def product_all(request):
+    return render(request, 'mysite/content_list.html')
+
+
+def product_popular(request):
+    return render(request, 'pages/product/product_popular.html')
+
+
+def product_sale(request):
+    return render(request, 'pages/product/product_sale.html')
+
+
+# 문의 관련
+def contact_intro(request):
+    return render(request, 'pages/contact/contact_intro.html')
+
+
+def contact_faq(request):
+    return render(request, 'pages/contact/contact_faq.html')
+
+
+def contact_qna(request):
+    return render(request, 'pages/contact/contact_qna.html')
+
+
+def contact_us(request):
+    return render(request, 'pages/contact/contact_us.html')
+
