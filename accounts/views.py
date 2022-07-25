c20cf9074fb8a4a2b07f4f5f00bd439960eca59b
05Ri <yh980827@naver.com>
2022-07-25 18:47:55 +0900

    4주차 댓글기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from accounts.forms import SignupForm
